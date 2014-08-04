e72aabf9f9ea2d52b598ef34a71fa9aac6b96098
Jake Fonseca <jafonseca@cs.uri.edu>
2014-08-04 13:45:42 -0400

    added team event api call

--- lib/OCCPGameServer/webservices.rb
+++ lib/OCCPGameServer/webservices.rb
@@ -181,6 +181,86 @@ module OCCPGameServer
             res
         end
 
+=begin
+    @api {get} /teams/<teamid>/events/<eventuid> Read team event
+    @apiVersion 0.1.0
+    @apiName GetTeamEvent
+    @apiGroup Teams
+
+    @apiSuccess {String} uuid Unique ID of event isntance
+    @apiSuccess {String} guid Registry ID of event type
+    @apiSuccess {String} name Name of the event
+    @apiSuccess {String} handler Handler class for the event
+    @apiSuccess {String} starttime '''GameTime''' start time for the event
+    @apiSuccess {String} endtime '''GameTime''' end time for the event
+    @apiSuccess {Number} frequency The number of seconds to elapse between successive events
+    @apiSuccess {Number} drift The number of seconds (+/-)to drift from the expected execution time
+    @apiSuccess {String} ipaddresspool IP address assignment pool
+    @apiSuccess {Object[]} scores Score items associated with this event
+    @apiSuccess {String} scores.scoregroup Score group label for this score
+    @apiSuccess {String} scores.points Number of points to assign for this score
+    @apiSuccess {Boolean} scores.onsuccess Whether to assign points when event succeeds or fails
+
+
+    @apiSuccessExample Success-Response:
+        HTTP/1.1 200 OK
+        {
+            "uuid" : "123456-1234-123456",
+            "guid" : "q-w-e",
+            "name" : "ping",
+            "handler" : "exec-handler-1",
+            "starttime" : "00:00:00",
+            "endtime" : "00:00:00",
+            "frequency" : "2",
+            "drift" : "0",
+            "ipaddresspool" : "pub_1",
+            "scores" : [	
+                { "score-group" : "redteam", "points" : "-13", "onsuccess" : "false" },
+                { "score-group" : "blueteam", "points" : "13", "onsuccess" : "true" }
+                ]
+        }
+
+    @apiError (Error 4xx) TeamNotFound The team for the given id was not found
+    @apiError (Error 4xx) EventNotFound The event for the given id was not found
+    @apiErrorExample Error-Response:
+        HTTP/1.1 404 NOT FOUND
+        {
+            "error" : "TeamNotFound"|"EventNotFound"
+        }
+=end
+        get '/teams/:id/events/:eventuid/' eo
+            teaminfo = nil
+            events = []
+            $appCore.teams.each do |team|
+                if String(team.teamid) == params[:id]
+                    teaminfo = true
+                    #Iterate through each list of events of the team
+                    team.singletonList.each do |event|
+                        if event.eventuid == params[:eventuid]
+                            events = event.wshash
+                            break
+                        end
+                    end
+                    if events.empty?
+                        team.periodicList.each do |event|
+                            if event.eventuid == params[:eventuid]
+                                events = eveht.wsHash
+                                break
+                            end
+                        end
+                    end
+                    break
+                end
+            end
+            if teaminfo.nil?
+                res = [404, {:error=>"TeamNotFound"}.to_json]
+            elsif events.empty?
+                res = [404, {:error=>"EventNotFound"}.to_json]
+            else
+                res = JSON.generate(events)
+            end
+            res
+        end
     end #End Class
 
 end #End Module
