1b83fad5df6a74ec4045a39b7b44834da888215b
Jake Fonseca <jafonseca@cs.uri.edu>
2015-04-20 17:18:20 -0400

    upgraded email handler to new event interface

--- lib/OCCPGameServer/Handlers/emailhandler.rb
+++ lib/OCCPGameServer/Handlers/emailhandler.rb
@@ -67,57 +67,26 @@ class EmailHandler < Handler
 
         Log4r::NDC.push('EmailHandler:')
         
-        # setup the execution space
-        # IE get a network namespace for this execution for the given IP address
-        ipPool = app_core.get_ip_pool(event.ipaddress)
-        if ipPool.nil? || ipPool.empty?
-            raise ArgumentError, "event ip address pool not defined"
-        end
-        
-        netLink = app_core.get_network(ipPool[:network])[:name]
-        if netLink.nil? || netLink.empty?
-            raise ArgumentError, "event ip address pool interface not defined"
-        end
        
-        ipAddr = ipPool[:addresses][rand(ipPool[:addresses].length)]
-        netInfo = {:iface => netLink, :ipaddr => ipAddr , :cidr => ipPool[:cidr], :gateway => ipPool[:gateway] }
-        #print "net info #{netInfo}"
-        begin
-            netNS = app_core.get_netns(netInfo) 
-        rescue ArgumentError => e
-            msg = "unable to create network namespace for event #{e}"
-            print msg.red
-            $log.error msg.red
-        end
-
         # Re-generate the message, really for a current timestamp and unique Message-ID field
         event.command = event.get_command
-        
-        # Prep the events command
-        newCom = netNS.comwrap(event.command)
-
-        gameTimeStart = $appCore.gameclock.gametime
+        newCom = event.command
 
         begin
             # run the provided command
             success = system(newCom, [:out, :err]=>'/dev/null')
         
         rescue Exception => e
-            app_core.release_netns(netNS.nsName)
             msg = "Event failed to run: #{e.message}".red
             $log.warn msg
         end
            
         returnValue = $?.exitstatus
 
-        gameTimeEnd = $appCore.gameclock.gametime
-        app_core.release_netns(netNS.nsName)
-        
-        #Log message that the event ran
-        msgHash = {:handler => 'EmailHandler', :eventname => event.name, :eventuid => event.eventuid, :custom => event.command,
-                    :starttime => gameTimeStart, :endtime => gameTimeEnd }
-        
         $log.debug "#{event.eventuid.light_magenta} returned #{returnValue} after executing #{event.command}"
+
+        returnScores = Array.new 
+        status = UNKNOWN
 
         if( success === nil )
                 msg = "Command failed to run: #{event.command}"
@@ -127,22 +96,25 @@ class EmailHandler < Handler
         elsif( returnValue === 0 ) #OK
 
             $log.info "#{event.name} #{event.eventuid.light_magenta} " + @@nagiosStatus[returnValue].green
-            app_core.INBOX << GMessage.new({:fromid=>'EmailHandler',:signal=>'EVENTLOG', :msg=>msgHash.merge({:status => 'OK'}) })
+            status = SUCCESS
+
         elsif( [1,2,3].include?(returnValue) )
             $log.debug "#{event.name} #{event.eventuid.light_magenta} " + @@nagiosStatus[returnValue].light_red
-            app_core.INBOX << GMessage.new({:fromid=>'EmailHandler',:signal=>'EVENTLOG', :msg=>msgHash.merge({:status => 'FAILED'}) })
+            status = FAILURE
         else            
             $log.error "#{event.eventuid.light_magenta} returned #{returnValue} after executing #{event.command}".light_red
+            status = UNKNOWN
         end
 
         # Record Scores to Database
         event.scores.each {|score|
             if score[:status] === returnValue 
-                app_core.INBOX << GMessage.new({:fromid=>'EmailHandler',:signal=>'SCORE', :msg=>score.merge({:eventuid => event.eventuid})})
+                returnScores << score
             end
         }
 
         Log4r::NDC.pop
+        return {:status => status, :scores => returnScores, :handler => 'ExecHandler2', :custom=> event.command}
 
     end
 
