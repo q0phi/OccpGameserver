00bc6f4469939d60c49e69300306d007611f5c59
Jake Fonseca <jafonseca@cs.uri.edu>
2015-04-20 16:07:16 -0400

    upgraded to new style handling

--- lib/OCCPGameServer/Handlers/nagiospluginhandler.rb
+++ lib/OCCPGameServer/Handlers/nagiospluginhandler.rb
@@ -74,39 +74,13 @@ class NagiosPluginHandler < Handler
 
         Log4r::NDC.push('NagiosPluginHandler:')
         
-        if event.ipaddress != nil
-            ipPool = app_core.get_ip_pool(event.ipaddress)
-            if ipPool[:ifname] != nil 
-                ipAddr = ipPool[:addresses][rand(ipPool[:addresses].length)]
-                netInfo = {:iface => ipPool[:ifname], :ipaddr => ipAddr , :cidr => ipPool[:cidr], :gateway => ipPool[:gateway] }
-                begin
-                    netNS = app_core.get_netns(netInfo) 
-                rescue ArgumentError => e
-                    msg = "unable to create network namespace for event #{e}; aborting execution"
-                    print msg.red
-                    $log.error msg.red
-                    return
-                end
-            else
-                $log.debug "Event #{event.name} ip address pool does not define an interface; local exec only"
-            end
-        end
-
-        gameTimeStart = $appCore.gameclock.gametime
-
         #TODO Optimize command specialization to arrays
         begin
-             # Change to the correct network namespace
-            fd = IO.sysopen('/var/run/netns/' + netNS.nsName, 'r')
-            success = $setns.call(fd, 0)
-            
-            raise ArgumentError, 'could not change to correct namespace' if success != 0
 
             # run the provided command
             success = system(event.command, [:out, :err]=>'/dev/null')
         
         rescue Exception => e
-            app_core.release_netns(netNS.nsName)
             msg = "Event failed to run: #{e.message}".red
             $log.warn msg
         end
@@ -116,28 +90,28 @@ class NagiosPluginHandler < Handler
         else
             returnValue = $?.exitstatus
         end
+        $log.debug "#{event.eventuid.light_magenta} executed #{event.command}"
         
-        gameTimeEnd = $appCore.gameclock.gametime
-        app_core.release_netns(netNS.nsName)
-        
-        #Log message that the event ran
-        msgHash = {:handler => 'NagiosPluginHandler', :eventname => event.name, :eventid => event.eventid, :eventuid => event.eventuid, :custom => event.command,
-                    :starttime => gameTimeStart, :endtime => gameTimeEnd }
-        
+        returnScores = Array.new 
+        status = UNKNOWN
+ 
         $log.debug "#{event.eventuid.light_magenta} returned #{returnValue} after executing #{event.command}"
         
         if( success === nil )
-                msg = "Command failed to run: #{event.command}"
-                $log.error(msg)
-                Log4r::NDC.pop
-                return nil
+            msg = "Command failed to run: #{event.command}"
+            $log.error(msg)
+            status = UNKNOWN
+        
         elsif( returnValue === 0 ) #OK
 
             $log.info "#{event.name} #{event.eventuid.light_magenta} " + @@nagiosStatus[returnValue].green
-            app_core.INBOX << GMessage.new({:fromid=>'NagiosPluginHandler',:signal=>'EVENTLOG', :msg=>msgHash.merge({:status => 'OK'}) })
+            status = SUCCESS
+        
         elsif( [1,2,3].include?(returnValue) ) 
+        
             $log.debug "#{event.name} #{event.eventuid.light_magenta} " + @@nagiosStatus[returnValue].light_red
-            app_core.INBOX << GMessage.new({:fromid=>'NagiosPluginHandler',:signal=>'EVENTLOG', :msg=>msgHash.merge({:status => 'FAILED'}) })
+            status = FAILURE
+
         else
             $log.error "Nagios plugin horrific failure".light_red
         end
@@ -145,12 +119,12 @@ class NagiosPluginHandler < Handler
         # Record Scores to Database
         event.scores.each {|score|
             if score[:status] === returnValue 
-                app_core.INBOX << GMessage.new({:fromid=>'NagiosPluginHandler',:signal=>'SCORE', :msg=>score.merge({:eventuid => event.eventuid})})
+                returnScores << score 
             end
         }
 
         Log4r::NDC.pop
-
+        return {:status => status, :scores => returnScores, :handler => 'NagiosPluginHandler', :custom=> event.command}
     end
 
 end #end class
